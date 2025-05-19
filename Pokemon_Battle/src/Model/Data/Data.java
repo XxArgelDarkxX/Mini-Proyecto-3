3cb5c4e52a20bff4c13460a73600dbe15614c603
KarenY93 <karen.sanabria@correounivalle.edu.co>
2025-05-19 17:20:08 -0500

    Modelo

---
+++ Pokemon_Battle/src/Model/Data/Data.java
@@ -0,0 +1,10 @@
+package Model;
+
+public class Data {
+
+    public static void initializeMoves() {
+        
+        throw new UnsupportedOperationException("Unimplemented method 'initializeMoves'");
+    }
+
+}
--- Pokemon_Battle/src/Modelo/Data/Data.java
+++
@@ -1,5 +0,0 @@
-package Modelo.Data;
-
-public class Data {
-
-}
